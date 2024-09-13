608406c80abed39779c89b15823f87fd27d811bf
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-09-14 00:51:06 +0300

    Add files via upload

--- LazackAdvanced.js
+++ LazackAdvanced.js
@@ -16530,7 +16530,7 @@ ${themeemoji} Title: ${result.title}`;
 	  if (!text) return replygcxeon(`You need to give the URL of Any Instagram video, post, reel, image`)
   let res
   try {
-    res = await fetch(`https://api.fgmods.xyz/api/downloader/igdl?url=${text}`)
+    res = await fetch(`https://api.fgmods.xyz/api/downloader/igdl?url=${text}&apikey=guYaDdtr`)
   } catch (error) {
     return replygcxeon(`An error occurred: ${error.message}`)
   }
