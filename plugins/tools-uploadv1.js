6e9c77768956431a3277d60a198a416f35b7cd2f
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-11-10 23:32:34 +0300

    MTAJU

--- plugins/tools-uploadv1.js
+++ plugins/tools-uploadv1.js
@@ -1,37 +1,42 @@
-const axios = require("axios")
-const FormData = require("form-data")
-const { fileTypeFromBuffer } = require("file-type")
+const axios = require("axios");
+const FormData = require("form-data");
+const { fileTypeFromBuffer } = require("file-type");
 
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || ''
-  if (!mime) return m.reply('Tidak ada media yang ditemukan')
-  let media = await q.download()
-  let result = await uploadPomf2(media)
+  let quotedMessage = m.quoted ? m.quoted : m;
+  let mimeType = (quotedMessage.msg || quotedMessage).mimetype || '';
+  
+  if (!mimeType) return m.reply('No media found');
+  
+  let media = await quotedMessage.download();
+  let result = await uploadToPomf2(media);
+  
   try {
-    m.reply(`*[ Uploader Pomf2 ]*
+    m.reply(`*[ Pomf2 Uploader ]*
 
 *Name:* ${result.files[0].name}
 *Link:* ${result.files[0].url}
 *Size:* ${result.files[0].size}
-*Expired:* Not expired date`)
+*Expired:* Not expired date`);
   } catch (e) {
-    return m.reply(e)
+    return m.reply(e);
   }
 }
-handler.help = ['up']
-handler.tags = ['tools']
-handler.command = /^(up)$/i
-handler.limit = true
 
-module.exports = handler
+handler.help = ['up'];
+handler.tags = ['tools'];
+handler.command = /^(up)$/i;
+handler.limit = true;
 
-async function uploadPomf2(media) {
+module.exports = handler;
+
+async function uploadToPomf2(media) {
   return new Promise(async (resolve, reject) => {
     const formData = new FormData();
     formData.append('files[]', media, { 
       filename: new Date() * 1 + '.jpg' 
     });
+    
     await axios.post('https://pomf2.lain.la/upload.php', formData, {
       headers: {
         ...formData.getHeaders(),
@@ -41,7 +46,7 @@ async function uploadPomf2(media) {
       resolve(response.data);
     })
     .catch((error) => {
-      resolve(e?.response)
+      resolve(error?.response);
     });
-  })
+  });
 }
