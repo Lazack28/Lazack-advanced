2663c0cb0bf080c918c4926681e70878573c5f90
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-11-11 15:12:58 +0300

    MTAJU

--- plugins/tools-amv.js
+++ plugins/tools-amv.js
@@ -2,24 +2,10 @@ const loli = new (require('@fainshe/scrapers'))
 
 module.exports = {
   run: async (m, { conn, Func }) => {
-    // React to the message with a clock emoji
-    m.react('🕒');
-
-    // Send a waiting message
-    let waitingMessage = await conn.sendMessage(m.chat, 'Please wait, fetching the AMV...', { quoted: m });
-
-    // Fetch a random anime source
-    let anu = await Func.random(['inferiordom', 'blubz_amv', '.kumui', 'yow_ph']);
-    let search = await loli.tiktokSearch(anu);
-
-    // Edit the waiting message to show the success message
-    await conn.editMessage(waitingMessage.key, {
-      text: `Here is your AMV: ${search.result.title}`,
-      buttons: [{ buttonId: 'view', buttonText: { displayText: 'View AMV' }, type: 1 }],
-    });
-
-    // Send the AMV file
-    conn.sendFile(m.chat, search.result.media.nowm, 'amv.mp4', search.result.title, m);
+    m.react('🕒')
+    let anu = await Func.random(['Lazack_28', 'Lazack_MD', 'stance_king8.8a', 'senegalreekk'])
+    let search = await loli.tiktokSearch(anu)
+    conn.sendFile(m.chat, search.result.media.nowm, 'amv.mp4', search.result.title, m)
   },
   help: ['amv'],
   tags: ['tools'],
