92e073f17ce12d7e41df130ec4453cf973a19f77
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-12-04 03:53:32 +0300

    mtaju

--- plugins/owner-blocklist.js
+++ plugins/owner-blocklist.js
@@ -2,7 +2,7 @@ let fetch = require("node-fetch")
 
 let handler = async (m, { conn }) => {
   let block = await conn.fetchBlocklist()                    
-  conn.reply(m.chat, '*Daftar List Yang Di Block*\n\n' + `Total: ${block == undefined ? '*0* Diblokir' : '*' + block.length + '* Diblokir'}\n` + block.map(v => '*-* @' + v.replace(/@.+/, '')).join`\n`, m, { mentions: block })
+  conn.reply(m.chat, '*LIST OF BLOCKED*\n\n' + `Total: ${block == undefined ? '*0* Blocked' : '*' + block.length + '* Diblokir'}\n` + block.map(v => '*-* @' + v.replace(/@.+/, '')).join`\n`, m, { mentions: block })
 }
 handler.help = ['blocklist']
 handler.tags = ['owner']
