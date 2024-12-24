c43417e2eb10e029e3185bf140e057fdc1349ea5
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-12-24 09:34:08 +0300

    mtaju

--- plugins/miscs-price.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  run: async (m, { conn, usedPrefix }) => {
-    conn.reply(m.chat, `🏷️ Upgrade to premium plan only Tsh 1500,- to get unlimited limits for 700 amount chat, you will be able to command the bot at my inbox.\n\nIf you want to buy contact *${usedPrefix}owner*`, m)
-  },
-  help: ['premium'],
-  tags: ['miscs'],
-  command: /^(premium)$/i
-}
