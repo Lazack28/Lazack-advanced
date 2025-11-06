6e9b876f13ccd9a203948fd5fe9de254653ed073
Lazack <164275612+Lazack28@users.noreply.github.com>
2025-11-06 13:50:13 +0300

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,16 @@
+import fs from "fs";
+import chalk from "chalk";
+
+global.owner = ["255734980103"];
+global.mode = false;
+
+global.namebotz = "Lazack Advance";
+global.packname = 'lazacksolutions';
+global.nameown = "Lazack | Projects";
+global.author = 'https://www.github.com/Lazack28';
+global.footer = "LAZACK-SOLUTIONS";
+
+global.YouTube = "https://www.youtube.com/@lazacksolutions";
+global.GitHub = "https://github.com/Lazack28";
+global.Telegram = "https://t.me/hackers_reality";
+global.ChannelWA = "https://whatsapp.com/channel/";
